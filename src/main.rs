fb4ad670abf51ca1227e4e92bba89b4d899eb779
yui019 <yui36189@gmail.com>
2025-03-09 21:17:04 +0100

    Set up TUI with cursive

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,69 @@
-use doko::{levenshtein, run_task_with_timer};
+use cursive::{
+    theme::Theme,
+    view::{Nameable, Resizable},
+    views::{Dialog, EditView, LinearLayout, TextView},
+};
+use doko::levenshtein;
 use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 use walkdir::WalkDir;
+
+fn get_filtered_files<'a>(files: &'a Vec<String>, query: &str) -> Vec<&'a String> {
+    let filtered_files: Vec<&String> = files
+        .par_iter()
+        .filter(|&file| levenshtein(&file, &query) < 2)
+        .collect();
+
+    return filtered_files;
+}
+
+fn set_linear_layout_children(linear_layout: &mut LinearLayout, files: Vec<&String>) {
+    linear_layout.clear();
+
+    for file in &files[..20] {
+        linear_layout.add_child(TextView::new(*file));
+    }
+}
 
 fn main() {
     let mut files: Vec<String> = vec![];
 
-    run_task_with_timer("index", || {
-        for entry in WalkDir::new("/home/haris/") {
-            files.push(entry.unwrap().file_name().to_str().unwrap().to_string());
-        }
+    for entry in WalkDir::new("/home/haris/") {
+        files.push(entry.unwrap().file_name().to_str().unwrap().to_string());
+    }
 
-        println!("Total files found: {}", files.len());
-    });
+    let mut siv = cursive::default();
+    siv.set_theme(Theme::terminal_default());
 
-    println!("----");
+    // TODO: use this to filter files outside the main thread!! (right now it's a line that does nothing lolz)
+    siv.cb_sink();
 
-    let query = "main.cpp";
+    siv.add_layer(
+        LinearLayout::vertical()
+            .child(
+                LinearLayout::vertical()
+                    .with_name("files_linear_layout")
+                    .full_height(),
+            )
+            .child(
+                Dialog::new().title("Search files").content(
+                    EditView::new()
+                        .on_submit(|s, _| s.quit())
+                        .on_edit(move |s, text, _| {
+                            let filtered_files = get_filtered_files(&files, text);
 
-    run_task_with_timer("search", || {
-        let filtered_files: Vec<&String> = files
-            .par_iter()
-            .filter(|&file| levenshtein(&file, &query) < 2)
-            .collect();
+                            s.call_on_name(
+                                "files_linear_layout",
+                                move |linear_layout: &mut LinearLayout| {
+                                    set_linear_layout_children(linear_layout, filtered_files)
+                                },
+                            );
+                        })
+                        .with_name("query_edit_view")
+                        .full_width(),
+                ),
+            )
+            .full_height(),
+    );
 
-        println!(
-            "Files matching \"{}\" found: {}",
-            query,
-            filtered_files.len()
-        );
-    });
+    siv.run();
 }
